3ce75131a3f07e26533b7c918f127e4a911399f2
tipiso <tipiso@gmail.com>
2019-05-15 12:28:38 +0200

    Updates

---
+++ precache-manifest.cc64bcb1e812292c911317016d881c44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a740de2f9217a3e506c314f486f2aef",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "24c82149b650e2bef54b",
+    "url": "/todoapp/static/css/main.00b181f1.chunk.css"
+  },
+  {
+    "revision": "7c56c16dada630ef73f3",
+    "url": "/todoapp/static/js/2.bd3cec10.chunk.js"
+  },
+  {
+    "revision": "24c82149b650e2bef54b",
+    "url": "/todoapp/static/js/main.e223d844.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
